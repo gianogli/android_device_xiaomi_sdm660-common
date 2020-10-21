9e80e5f2215d5786195a16f1f9de87d0e4c05bcf
wayneeg <waynee@outlook.com.br>
2020-10-21 18:53:47 -0300

    sdm660-common: set GCamGo default
    Signed-off-by: wayneeg <waynee@outlook.com.br>

--- sdm660.mk
+++ sdm660.mk
@@ -476,3 +476,8 @@ PRODUCT_BOOT_JARS += \
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
+
+# Prebuilt Packages
+PRODUCT_PACKAGES += \
+    GCamGo
+

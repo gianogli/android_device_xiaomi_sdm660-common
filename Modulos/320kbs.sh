f866912a24f1969f7d2e6638d49a1ff385ec57cc
chrisw444 <wandersonrodriguesf1@gmail.com>
2020-12-25 22:49:37 +0000

    sdm660-common: Enable Audio Bitrate Increase 320kbs
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

---
+++ Modulos/320kbs.sh
@@ -0,0 +1,15 @@
+#!/system/bin/sh
+
+ui_print " "
+ui_print "320kb/s bitrate for video"
+
+ui_print "kewb"
+
+
+DEVICE=`getprop ro.product.device`
+
+ui_print "Device: "$DEVICE""
+
+ui_print "bruh "
+ ui_print "Installing for "$DEVICE" ..."
+

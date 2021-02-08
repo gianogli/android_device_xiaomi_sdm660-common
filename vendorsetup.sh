82e4b725008ef49a207366a916b7237966a40a5d
ronald-b <ronaldsantospetersen@gmail.com>
2021-02-08 10:29:59 -0400

    sdm660-common: removed vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,4 +0,0 @@
-rm -rf hardware/qcom-caf/msm8998
-git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
-git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/media
-git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/display
